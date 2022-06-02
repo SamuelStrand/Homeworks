03cd7835e9f0e1659c37c48c986187cb75dff205
SamuelStrand <105794723+SamuelStrand@users.noreply.github.com>
2022-06-02 23:23:24 +0600

    Uploaded homework. Json (both tasks and Temp directory)

---
+++ get_one.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+url = "https://api.instantwebtools.net/v1/airlines/1"
+response = requests.get(url)
+json_data = response.content.decode()
+airlines = json.loads(json_data)
+with open('Temp/get_one.json', 'w') as file:
+    json.dump(json_data, file)
+print(json_data)
+print(type(json_data))
---
+++ get_one2.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+url = "https://api.instantwebtools.net/v1/airlines/"
+n = 0
+new_url = ""
+
+for num in range(1, 100+1):
+    for number in range(0, 1):
+        n += 1
+    new_url = f'{url}{n}'
+    print(new_url)
+    response = requests.get(new_url)
+    new_url = response.content.decode()
+    with open(file=f"Temp/get_many_{num}.json", mode="w", encoding="cp1251") as file:
+        json.dump(new_url, file)
+# airlines = json.loads(new_url)
+# print(type(airlines))
+# print(airlines)
+# print(type(new_url))
+# print(new_url)
